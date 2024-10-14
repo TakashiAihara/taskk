21200352beff2c850d19a259bd2aebff7547d0dd
TakashiAihara <47384567+TakashiAihara@users.noreply.github.com>
2024-10-14 02:22:27 +0000

    implement delete feature

--- src/app/_components/tasks/task.tsx
+++ src/app/_components/tasks/task.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import { type Task as TaskType } from "#/generated/zod";
-import { Calendar, CheckSquare, Clock } from "lucide-react";
+import { Calendar, ListTodo, Clock, Trash } from "lucide-react";
 import { api } from "@/trpc/react";
 
 export const Task: React.FC<TaskType> = ({
@@ -22,6 +22,12 @@ export const Task: React.FC<TaskType> = ({
     },
   });
 
+  const deleteTask = api.task.delete.useMutation({
+    onSuccess: async () => {
+      await utils.task.invalidate();
+    },
+  });
+
   const formatDate = (date: Date | null | undefined) => {
     return date ? new Date(date).toLocaleDateString() : 'Not set';
   };
@@ -30,21 +36,28 @@ export const Task: React.FC<TaskType> = ({
     updateTask.mutate({ id, completed: e.target.checked });
   };
 
+  const handleDelete = () => {
+    deleteTask.mutate({ id });
+  };
+
   return (
     <div className="w-full max-w-md bg-white shadow-lg rounded-lg overflow-hidden border border-gray-200">
       <div className="px-4 py-5 sm:p-6">
-        <div className="flex items-center mb-2">
-          <span className="text-lg font-medium text-gray-500 mr-2">{`<${id}>`}</span>
-          <h3 className="text-lg leading-6 font-medium text-gray-900">{title}</h3>
-        </div>
+        <div className="flex items-center justify-between mb-2">
+          <div className="flex items-center mb-2">
+            <input
+                type="checkbox"
+                checked={completed}
+                onChange={handleCompletedChange}
+                className="form-checkbox h-5 w-5 text-blue-600 mr-2"
+              />
+              <span className="text-sm font-medium text-gray-500 mr-2">{`${id}:`}</span>
+              <h3 className="text-lg leading-6 font-medium text-gray-900">{title}</h3>
+            </div>
+            <Trash className="" onClick={handleDelete}/>
+          </div>
         <div className="mt-3 flex items-center text-sm text-gray-500">
-          <input
-            type="checkbox"
-            checked={completed}
-            onChange={handleCompletedChange}
-            className="form-checkbox h-5 w-5 text-blue-600 mr-2"
-          />
-          <CheckSquare className={`mr-1.5 h-5 w-5 flex-shrink-0 ${completed ? 'text-green-500' : 'text-gray-400'}`} />
+         <ListTodo className={`mr-1.5 h-5 w-5 flex-shrink-0 ${completed ? 'text-green-500' : 'text-gray-400'}`} />
           <p>{completed ? 'Completed' : 'In Progress'}</p>
         </div>
         <div className="mt-3 flex items-center text-sm text-gray-500">
